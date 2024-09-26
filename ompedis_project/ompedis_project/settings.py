798e3c28bb5abdea87797477b5d27fb7128d50e9
lcayaxp <lcayaxp@miumg.edu.gt>
2024-09-25 22:20:27 -0600

    configurando la recuperación de contraseñas con verificación de 2 pasos

--- ompedis_project/ompedis_project/settings.py
+++ ompedis_project/ompedis_project/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'luiscayax230316@gmail.com'
-EMAIL_HOST_PASSWORD = 'Soygu@poyque1'
+EMAIL_HOST_USER = 'lcayaxp@miumg.edu.gt'
+EMAIL_HOST_PASSWORD = 'hafr duhd jlue avgm'
