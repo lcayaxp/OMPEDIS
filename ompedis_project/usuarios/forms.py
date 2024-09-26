5fdb38867647207f7c8000d0b203d4505d8c1364
lcayaxp <lcayaxp@miumg.edu.gt>
2024-09-25 21:46:18 -0600

    asignación de campos nombre y apellido en usuarios

--- ompedis_project/usuarios/forms.py
+++ ompedis_project/usuarios/forms.py
@@ -1,3 +1,4 @@
+# forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser, ROLES_CHOICES
@@ -6,17 +7,22 @@ from django.contrib.auth.forms import AuthenticationForm
 # Formulario para la creación de usuarios
 class UsuarioCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tu correo electrónico'}))
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tu nombre'}))
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tu apellido'}))
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('email', 'rol', 'departamento')
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email', 'rol', 'departamento')
+
 # Formulario para la modificación de usuarios
 class UsuarioChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'rol', 'departamento']
+        fields = ['username', 'first_name', 'last_name', 'email', 'rol', 'departamento']
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'rol': forms.Select(attrs={'class': 'form-control'}),
             'departamento': forms.TextInput(attrs={'class': 'form-control'}),
@@ -26,7 +32,7 @@ class UsuarioChangeForm(UserChangeForm):
 class PerfilUsuarioForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'rol', 'departamento']
+        fields = ['username', 'first_name', 'last_name', 'email', 'rol', 'departamento']
 
 # Formulario para el login
 class CustomAuthenticationForm(AuthenticationForm):
