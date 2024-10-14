258031307331ee0dc9c669319ec7594e8b19bd0d
lcayaxp <lcayaxp@miumg.edu.gt>
2024-10-13 22:05:41 -0600

    se borraron datos inncecesarios

--- ompedis_project/pacientes/models.py
+++ ompedis_project/pacientes/models.py
@@ -61,33 +61,3 @@ class Paciente(models.Model):
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
 
-# Modelo para representar un diagnóstico
-class Diagnostico(models.Model):
-    descripcion = models.TextField(blank=True, null=True)  # Descripción del diagnóstico (opcional)
-    tipo = models.CharField(max_length=50, default='General')  # Tipo de diagnóstico (por ejemplo, general, específico)
-
-    def __str__(self):
-        # Devuelve el tipo de diagnóstico como representación de la instancia
-        return f'Diagnóstico {self.tipo}'
-
-# Modelo para representar la historia clínica de un paciente
-class HistoriaClinica(models.Model):
-    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)  # Relación con el paciente
-    diagnostico = models.ForeignKey(Diagnostico, on_delete=models.CASCADE, default=1)  # Relación con el diagnóstico, asegurarse que el ID 1 exista
-    fecha_inicio = models.DateField(default=timezone.now)  # Fecha en la que se inicia la historia clínica
-    estado = models.CharField(max_length=50, default='Pendiente')  # Estado actual de la historia clínica (por ejemplo, pendiente, cerrado)
-
-    def __str__(self):
-        # Devuelve una representación indicando a quién pertenece la historia clínica
-        return f'Historia Clínica de {self.paciente.nombre}'
-
-# Elimina esta clase
-# class Responsable(models.Model):
-#     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='responsables')
-#     nombre = models.CharField(max_length=100)
-#     apellido = models.CharField(max_length=100)
-#     parentesco = models.CharField(max_length=50)
-#     telefono = models.CharField(max_length=20)
-
-#     def __str__(self):
-#         return f'{self.nombre} {self.apellido} - {self.parentesco}'
