0176a1618ca19d7b23303d846533f0b91657665d
lcayaxp <lcayaxp@miumg.edu.gt>
2024-10-14 00:20:28 -0600

    se agregaron mensajes interactivos para permisos

--- ompedis_project/usuarios/decorators.py
+++ ompedis_project/usuarios/decorators.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseRedirect
+from django.urls import reverse  # Para redirigir a una URL específica
+from django.contrib import messages
 from functools import wraps
 
 # Decorador para permitir solo a la administrador
@@ -7,16 +9,20 @@ def administrador_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.rol == 'administrador':
             return view_func(request, *args, **kwargs)
-        return HttpResponseForbidden("No tienes permiso para acceder a esta vista.")
+        # Si no es administrador, mostrar mensaje de error y redirigir
+        messages.error(request, "No tienes permiso para esta acción")
+        return HttpResponseRedirect(reverse('dashboard'))  # Redirigir a la página principal o cualquier otra
     return _wrapped_view
 
-# Decorador para permitir solo a los trabajadores
+# Decorador para permitir solo a los moderadores
 def moderador_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.rol == 'moderador':
             return view_func(request, *args, **kwargs)
-        return HttpResponseForbidden("No tienes permiso para acceder a esta vista.")
+        # Si no es moderador, mostrar mensaje de error y redirigir
+        messages.error(request, "No tienes permiso para esta acción")
+        return HttpResponseRedirect(reverse('dashboard'))  # Cambia la URL según lo necesites
     return _wrapped_view
 
 # Decorador para permitir solo a los usuarios
@@ -25,14 +31,18 @@ def usuario_required(view_func):
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.rol == 'usuario':
             return view_func(request, *args, **kwargs)
-        return HttpResponseForbidden("No tienes permiso para acceder a esta vista.")
+        # Si no es usuario, mostrar mensaje de error y redirigir
+        messages.error(request, "No tienes permiso para esta acción")
+        return HttpResponseRedirect(reverse('dashboard'))
     return _wrapped_view
 
-
+# Decorador para permitir solo a moderadores o administradores
 def moderador_o_administrador_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.rol in ['moderador', 'administrador']:
             return view_func(request, *args, **kwargs)
-        return HttpResponseForbidden("No tienes permiso para acceder a esta vista.")
+        # Si no es moderador ni administrador, mostrar mensaje de error y redirigir
+        messages.error(request, "No tienes permiso para esta acción")
+        return HttpResponseRedirect(reverse('dashboard'))
     return _wrapped_view
